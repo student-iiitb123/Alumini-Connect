a825b0d8633be011d7f1859ddfe9e2a104d79cd6
Ashish singh Goutam <ashishgautam7696@gmail.com>
2025-12-31 23:08:50 +0530

    add triplets whose sum =0

--- OneDrive/Documents/Desktop/JAVA_DSA/Array/Triplet.java
+++ OneDrive/Documents/Desktop/JAVA_DSA/Array/Triplet.java
@@ -1,12 +1,20 @@
 public class Triplet {
  public static void main(String[] args) {
     int arr[] = {-1,0,1,2,-1,-4};
+  
 
     for(int i=0;i<arr.length;i++){
         for(int j=i+1;j<arr.length;j++){
+           
            for(int k =j+1;k<arr.length;k++){
-            System.out.println(arr[i] + "," + arr[j] + "," + arr[k]);
-           }
+          int sum = arr[i] + arr[j] + arr[k];
+          if(sum == 0){
+             System.out.println(arr[i] + "," + arr[j] + "," + arr[k]);
+          }
+           
+        }
+         
+        
         }
     }
 
